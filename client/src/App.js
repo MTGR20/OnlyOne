1c1100aa88f0e85af525aee6c3c73cdfe68571e6
rladbflaz <youro1228@naver.com>
2023-10-18 15:40:47 +0900

    Create LandingPage.js, LoginPage.js, RegisterPage.js, app.py, models.py / Update App.css, App.js, server.py

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,29 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-
+//C:\react-js\myreactdev\src\App.js
+import React, { } from 'react';
+import './App.css';
+  
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+  
+import LandingPage from "./pages/LandingPage";
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+ 
 function App() {
-  // state
-  const [data, setData] = useState([{}])
-
-  useEffect(() => 
-    {
-    	fetch("/users").then(
-          response => response.json()
-        ).then(
-          data => {
-            // 받아온 데이터를 data 변수에 update
-            setData(data);
-          }
-        ).catch(
-          (err) => console.log(err)
-        )
-    }, [])
-
   return (
-    <div className='App'>
-      <h1>test 하는 중...</h1>
-      <div>
-        {/* 삼항연산자 */}
-        { (typeof data.users === 'undefined') ? (
-          // fetch가 완료되지 않았을 경우에 대한 처리
-          <p>loding...</p>
-        ) : (
-          data.users.map((u) => <p>{u.name}</p>)
-        )}
-      </div>
+    <div className="vh-100 gradient-custom">
+    <div className="container">
+      <h1 className="page-header text-center">React and Python Flask Login Register</h1>
+   
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+    </div>
+  );
 }
-
+   
 export default App;
